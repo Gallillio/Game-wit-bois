c3cd94bdba68a03f71b222e97d2dcc92fe52dfa5
EyadTamer999 <eyadhassan2002@gmail.com>
2023-04-23 14:59:56 +0200

    added basic player mode switching

---
+++ Assets/Scripts/SwitchWorld.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWorld : MonoBehaviour
+{
+    public enum Mode
+    {
+        VECTOR,PIXEL
+    }
+    private Mode playerMode = Mode.PIXEL;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Joystick1Button5))
+        {
+            if (playerMode == Mode.PIXEL)
+            {
+                Debug.Log("Player is now in Vector");
+                playerMode = Mode.VECTOR;
+            }
+            else
+            {
+                Debug.Log("Plyer is now in Pixel");
+                playerMode = Mode.PIXEL;
+            }
+        }
+    }
+}
