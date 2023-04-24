1128081424fff17865c336d2d2930fb8b4227035
EyadTamer999 <eyadhassan2002@gmail.com>
2023-04-24 18:21:11 +0200

    Finished the code ig

--- Assets/Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -6,7 +6,8 @@ namespace InventorySystem
 {
     public class Inventory: MonoBehaviour
     {
-        public List<InventoryItem> inventory = new List<InventoryItem>();
+        public static event Action<List<InventoryItem>> OnInventoryChange; 
+        public List<InventoryItem> inventory = new List<InventoryItem>(9);
         private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>(); //if we have an existing item in our inventoru we'll add it to the current stacksize of the item we have, else we'll create a new key value(set to 1) to the item
 
         private void OnEnable()
@@ -27,14 +28,20 @@ namespace InventorySystem
                 //item exists then increment stack size
                 item.AddToStack();
                 print("total " + item.itemData.displayName + " " + item.stackSize);
+                OnInventoryChange?.Invoke(inventory);
             }
-            else
+            else  if (inventory.Count <= 9)
             {
+                //Quick fix
+                //if the inventory is full, then do not add to the inventory
+                // what i think will happen is the collectible will be deleted 
+                
                 //item does not exist so create new entry and add stack size
                 InventoryItem newItem = new InventoryItem(itemData);
                 inventory.Add(newItem);
                 itemDictionary.Add(itemData, newItem);
                 print("Added " + newItem.itemData.displayName + " " + newItem.stackSize + " for the first time");
+                OnInventoryChange?.Invoke(inventory);
 
             }
         }
@@ -50,6 +57,8 @@ namespace InventorySystem
                     inventory.Remove(item);
                     itemDictionary.Remove(itemData);
                 }
+                OnInventoryChange?.Invoke(inventory);
+
             }
         }
     }
---
+++ Assets/Scripts/InventorySystem/InventoryManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using InventorySystem;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    //we have 9 possible slots, so the list will be of size 9. Meaning 9 different items
+    public List<InventorySlot> inventorySlots = new List<InventorySlot>(9);
+    public GameObject inventoryPanel;
+
+    private void OnEnable()
+    {
+        Inventory.OnInventoryChange += DrawInventory;
+    }
+    
+    private void OnDisable()
+    {
+        Inventory.OnInventoryChange -= DrawInventory;
+    }
+    
+    void ResetInventroy()
+    {
+        foreach (Transform childTransform in transform)
+        {
+            Destroy((childTransform.gameObject));
+        }
+        inventorySlots = new List<InventorySlot>(9);
+    }
+
+    void DrawInventory(List<InventoryItem> inventory)
+    {
+        ResetInventroy();
+
+        for (int i = 0; i < inventorySlots.Capacity; i++)
+        {
+            CreateInventorySlot();
+        }
+
+        for (int i = 0; i < inventory.Capacity; i++)
+        {
+            inventorySlots[i].DrawSlot(inventory[i]);
+        }
+        
+    }
+    void CreateInventorySlot()
+    {
+        GameObject newSlot = Instantiate(slotPrefab);
+        newSlot.transform.SetParent(transform, false);
+
+        InventorySlot newSlotsComponent = newSlot.GetComponent<InventorySlot>();
+        newSlotsComponent.ClearSlot();
+        
+        inventorySlots.Add(newSlotsComponent);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //shows the inventory panel
+        if ((Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Joystick1Button7)))
+        {
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
