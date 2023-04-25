3486a6f0638e888e257d7f767e47799271d5d9bc
EyadTamer999 <eyadhassan2002@gmail.com>
2023-04-25 20:19:46 +0200

    added Obstacle damage thing

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -7,22 +7,11 @@ public class ObstacleManager : MonoBehaviour
 {
     public SwitchWorld playerMode;
     //each obstacle will have this script, and based on their tag, they'll hurt the player or do nothing to the player
-
-    private void Update()
+    
+    private void OnCollisionStay2D(Collision2D target)
     {
-        // print(gameObject.tag.ToString().ToUpper());
-    }
-
-
-    private void OnCollisionStay(Collision target)
-    {
-      
-        
-        //STILL NOT COMPLETE
-        
-        print("dont touch me");
-        
         //if this gameobject and the target are the same type, then you good bro, else you're damaged
+        // todo damamge player health at the else statement block 
         if (gameObject.CompareTag(target.gameObject.tag) || (target.gameObject.tag.Equals("Player") && playerMode.currentPlayerMode.ToString().Equals(gameObject.tag.ToUpper())))
         {
             print("you good bro");
